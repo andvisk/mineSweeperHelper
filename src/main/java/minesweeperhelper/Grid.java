1fb6fb164473feed4afb77c539f88df59aaa2b10
andriusvisk <andriusvisk@gmail.com>
2022-09-17 15:31:05 +0300

    .

--- src/main/java/minesweeperhelper/Grid.java
+++ src/main/java/minesweeperhelper/Grid.java
@@ -4,7 +4,7 @@ public class Grid {
 
     public static final int MIN_WIDTH = 9;
     public static final int MIN_HEIGHT = 9;
-    public static final int TOLLERANCE_IN_PERCENT = 20;
+    public static final int TOLLERANCE_IN_PERCENT = 50; // 25% to one direction
 
     private GridCell[][] grid;
 
--- src/main/java/minesweeperhelper/GridUtils.java
+++ src/main/java/minesweeperhelper/GridUtils.java
@@ -5,8 +5,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GridUtils {
+
+    private static Logger log = LogManager.getLogger(GridUtils.class);
 
     public static Grid collectGrid(List<GridCell> cells) {
 
@@ -41,32 +47,58 @@ public class GridUtils {
                 }
             }
 
+            int rows = -1;
             for (Integer x : mapByX.keySet()) {
                 mapByX.get(x).sort((a, b) -> Integer.compare(a.getRect().y, b.getRect().y));
+                if (rows < 0) {
+                    rows = mapByX.get(x).size();
+                } else {
+                    if (mapByX.get(x).size() != rows) {
+                        log.info("unable to get grid");
+                    }
+                }
             }
 
             List<Integer> xs = new ArrayList<>(mapByX.keySet());
             xs.sort((a, b) -> Integer.compare(a, b));
 
+            Grid grid = new Grid(xs.size(), mapByX.get(xs.get(0)).size());
+
             // check x's and y's for sequency
             Integer prevX = null;
+            int column = -1;
             for (Integer x : xs) {
+                ++column;
                 if (prevX != null
                         && Math.abs(x - (prevX + cellWidth)) > (double) cellWidth / 100 * Grid.TOLLERANCE_IN_PERCENT) {
+                    log.info("unable to get grid");
                     return null;
                 }
                 prevX = x;
 
-                //checking y's sequency
+                // checking y's sequency
                 Integer prevY = null;
-                for(Iteger y:map.get(x).){
-
+                List<GridCell> columnData = mapByX.get(x);
+                List<Integer> ys = columnData.stream().map(p -> p.getRect().y).collect(Collectors.toList());
+                ys.sort((a, b) -> Integer.compare(a, b));
+                int row = -1;
+                for (Integer y : ys) {
+                    ++row;
+                    if (prevY != null
+                            && Math.abs(y - (prevY + cellHeight)) > (double) cellHeight / 100
+                                    * Grid.TOLLERANCE_IN_PERCENT) {
+                        log.info("unable to get grid");
+                        return null;
+                    }
+                    grid.setCell(x, y, columnData.stream().filter(p->p.getRect().y == y).findFirst().get());
+                    prevY = y;
                 }
             }
 
-            return new Grid(xs.size(), mapByX.get(xs.get(0)).size());
+            return grid;
         }
 
+        log.info("unable to get grid");
         return null;
     }
 
