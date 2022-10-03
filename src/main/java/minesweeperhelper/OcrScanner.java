3804e2770c1e3ec5a3cee1b56535bc40d13f0497
Andrius <andriusvisk@gmail.com>
2022-10-03 18:01:14 +0300

    .

--- src/main/java/minesweeperhelper/OcrScanner.java
+++ src/main/java/minesweeperhelper/OcrScanner.java
@@ -1,15 +1,28 @@
 package minesweeperhelper;
 
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.leptonica.PIX;
+import org.bytedeco.tesseract.TessBaseAPI;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-
 public class OcrScanner {
 
     private static Logger log = LogManager.getLogger(OcrScanner.class);
+    private TessBaseAPI api;
+
+    public OcrScanner() {
+        TessBaseAPI api = new TessBaseAPI();
+        if (api.Init(null, "eng") != 0) {
+            log.error("Could not initialize tesseract.");
+            System.exit(1);
+        }
+    }
 
     public String getTextFromImage(Mat srcImg) {
 
@@ -17,8 +30,19 @@ public class OcrScanner {
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY);
         Imgproc.GaussianBlur(mat, mat, new Size(3, 3), 0);
         Imgproc.threshold(mat, mat, 0, 255, Imgproc.THRESH_OTSU);
-        
-        return "aaa";
-    }
 
+        try {
+            byte[] data = ((java.awt.image.DataBufferByte) ImageUtils.mat2BufferedImage(mat).getRaster()
+                    .getDataBuffer()).getData();
+
+            api.SetImage(data, mat.cols(), mat.rows(), mat.channels(), (int) mat.step1());
+
+            BytePointer outText = api.GetUTF8Text();
+            
+            return outText.getString();
+        } catch (IOException e) {
+
+        }
+        return null;
+    }
 }
