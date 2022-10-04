e928d4859da3b141a4d6e89452b89728044df4a8
Andrius <andriusvisk@gmail.com>
2022-10-04 08:42:20 +0300

    .

--- src/test/java/minesweeperhelper/OcrScannerTest.java
+++ src/test/java/minesweeperhelper/OcrScannerTest.java
@@ -12,8 +12,15 @@ public class OcrScannerTest {
         OcrScanner ocrScanner = new OcrScanner();
         Mat patternImage = Imgcodecs.imread("src/main/resources/" + 1 + ".png");
         String text = ocrScanner.getTextFromImage(patternImage);
+        ocrScanner.destructor();
 
         Assertions.assertNull(text);
         Assertions.assertSame("1", text);
+    }
+
+    @Test
+    public void testOcv(){
+        String prop = System.getProperty("java.library.path");
+        Mat patternImage = Imgcodecs.imread("src/main/resources/" + 0 + ".png");
     }
 }
