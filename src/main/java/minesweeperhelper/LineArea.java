aae18c844c13e88a2285c934d0317fe0ef232862
Andrius <andriusvisk@gmail.com>
2022-10-10 21:18:40 +0300

    .

--- src/main/java/minesweeperhelper/LineArea.java
+++ src/main/java/minesweeperhelper/LineArea.java
@@ -1,6 +1,7 @@
 package minesweeperhelper;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.UUID;
 
@@ -11,11 +12,16 @@ public class LineArea {
 
     public UUID id;
     public double[] coord;
-    public BigDecimal leftAngle;
+
+    public Point point1;
+    public Point point2;
+
     public BigDecimal length;
     public BigDecimal lengthDecreased;
 
     public ColorsEnum color;
+
+    public BigDecimal angleIndegrees;
 
     public LineArea() {
 
@@ -26,13 +32,40 @@ public class LineArea {
         this.id = UUID.randomUUID();
         this.coord = coord;
 
-        length = BigDecimal.valueOf(Math.sqrt(Math.pow(coord[3] - coord[1], 2) + Math.pow(coord[2] - coord[0], 2)))
-                .setScale(2);
+        double x1 = coord[0];
+        double x2 = coord[2];
+        double y1 = coord[1];
+        double y2 = coord[3];
 
-        Point topPoint = (coord[1] < coord[3]) ? new Point(coord[0], coord[1]) : new Point(coord[2], coord[3]);
-        Point steepAnglePoint = (coord[1] < coord[3]) ? new Point(coord[0], coord[1]) : new Point(coord[2], coord[3])
+        length = length(new Point(x1, y1), new Point(x2, y2));
 
+        lengthDecreased = length
+                .subtract(length.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_EVEN).multiply(tollerance));
 
+        point1 = new Point(x1, y1);
+        point2 = new Point(x2, y2);
+
+        Point topPoint = (y1 < y2) ? point1 : point2;
+        Point steepAnglePoint = (y1 < y2) ? new Point(x1, y2) : new Point(x2, y1);
+
+        BigDecimal lengthTopToSteepAngle = length(topPoint, steepAnglePoint);
+
+        angleIndegrees = BigDecimal.valueOf(Math
+                .toDegrees(Math.asin(lengthTopToSteepAngle.divide(length, 4, RoundingMode.HALF_EVEN).doubleValue()))).setScale(2, RoundingMode.HALF_EVEN);
+
+    }
+
+    public boolean isHorizontal(){
+        return angleIndegrees.equals(BigDecimal.valueOf(0).setScale(2));
+    }
+
+    public boolean isVertical(){
+        return angleIndegrees.equals(BigDecimal.valueOf(90).setScale(2));
+    }
+
+    private BigDecimal length(Point point1, Point point2) {
+        return BigDecimal.valueOf(Math.sqrt(Math.pow(point2.y - point1.y, 2) + Math.pow(point2.x - point1.x, 2)))
+                .setScale(2, RoundingMode.HALF_EVEN);
     }
 
 }
